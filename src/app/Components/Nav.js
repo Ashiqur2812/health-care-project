c45e535d68067bffcf275d061b86f67ba1e6ca82
mysha <maimunarahman30@gmail.com>
2025-03-30 03:27:37 +0600

    details route

--- src/app/Components/Nav.js
+++ src/app/Components/Nav.js
@@ -1,52 +1,69 @@
+"use client"
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
-
-
+// import SignInButton from './SignInButton';
 
 const Nav = () => {
-    return (
-        <div>
-       <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>HOME</a></li>
-       <Link href='/about'><li>ABOUT</li></Link>
-        <li><a>BLOGS</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">NORDIS</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    <li><a>HOME</a></li>
-    <Link href='/about'><li>ABOUT</li></Link>
-        <li><a>BLOGS</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn bg-[#A1EEBD] ">Sign Up</a>
-  </div>
-</div>
+  // const { data: session } = useSession();
 
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li><Link href="/">HOME</Link></li>
+              <li><Link href="/about">ABOUT</Link></li>
+              <li><a href="#">BLOGS</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">NORDIS</a>
         </div>
-    );
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link href="/">HOME</Link></li>
+            <li><Link href="/about">ABOUT</Link></li>
+            <li><a href="#">BLOGS</a></li>
+          </ul>
+        </div>
+
+        <div className="navbar-end flex gap-4">
+          <a className="btn bg-[#A1EEBD]">Sign Up</a>
+
+          {/* {session ? (
+            <>
+              <span className="font-medium">{session.user?.name}</span>
+              <button 
+                onClick={() => signOut()} 
+                className="bg-white text-orange-500 px-4 py-1 rounded">
+                Sign out
+              </button>
+            </>
+          ) : (
+            <SignInButton />
+          )} */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Activity from './Components/Activity';
 import Numeric from './Components/Numeric';
+import Expert from './Components/Expert';
 
 const page = () => {
     return (
         <div>
             <Activity/>
             <Numeric/>
+            <Expert/>
         </div>
     );
 };
