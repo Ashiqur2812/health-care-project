d43dbaa2ef4f83935ac8cdc23c6e985502a76400
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-04 20:49:07 +0600

    Update blogs page title and improve layout styling

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,7 +1,9 @@
 const Blogs = () => {
   return (
-    <div className="bg-white text-gray-900">
-      <h1> This is our Blogs Page</h1>
+    <div className="text-gray-900 w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto py-20">
+      <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-[#274760]">
+        Latest Health News & Tips
+      </h1>
     </div>
   );
 };
