789d4043b2e82b2dd5b5b39d3f65e514394c1ddb
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-04 20:23:11 +0600

    Enhance navigation and add blogs page; refactor doctor data fetching

--- src/app/api/doctors/get/route.js
+++ src/app/api/doctors/get/route.js
@@ -1,6 +1,6 @@
-import { connectMongodb } from "../../../../../lib/mongodb";
-import { DoctorModel } from "../../../../../model/doctorsModel/doctorsModel";
+import connectMongodb from "../../../../../lib/mongodb";
 import { NextResponse } from "next/server";
+import { DoctorModel } from "../../../../../model/doctorsModel/doctorsModel";
 
 export const GET = async () => {
   await connectMongodb();
---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,9 @@
+const Blogs = () => {
+  return (
+    <div className="bg-white text-gray-900">
+      <h1> This is our Blogs Page</h1>
+    </div>
+  );
+};
+
+export default Blogs;
