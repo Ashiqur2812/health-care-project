bbb7eaa0e05291e18f97bf1c53f459a8c0b1920d
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-04 23:55:01 +0600

    Refactor doctor API routes to use Mongoose for MongoDB connection and add console log for data retrieval

--- src/app/api/doctors/post/route.js
+++ src/app/api/doctors/post/route.js
@@ -1,11 +1,12 @@
 import { DoctorModel } from "../../../../../model/doctorsModel/doctorsModel";
 import { NextResponse } from "next/server";
 import { connectMongoString } from "../../../../../lib/mongodb";
+import mongoose from "mongoose";
 
 //create post route in doctors
 export const POST = async (req) => {
   const payload = await req.json();
-  await connectMongoString();
+  await mongoose.connect(connectMongoString);
   const result = new DoctorModel(payload);
   await result.save();
   return NextResponse.json({ success: true, status: 200 });
