6d70e9602fa84076e694af4ba68b17a44d5c2ecf
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-05 10:59:20 +0600

    blogs card design update

--- src/app/Components/BlogCard/BlogCard.jsx
+++ src/app/Components/BlogCard/BlogCard.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 const BlogCard = ({ blog }) => {
-  console.log(blog);
-  const { title, description, image } = blog || {};
+  const { title, description, image, date } = blog || {};
   return (
     <div>
-      <Image src={image} alt={title} width={400} height={400}></Image>
+      <div className="w-full h-[400px] relative mb-4">
+        <Image src={image} alt={title} fill className="object-cover"></Image>
+      </div>
+      <h3>{date}</h3>
       <h2 className="text-2xl font-bold text-[#274760]">{title}</h2>
       <p className="text-gray-700">{description}</p>
     </div>
