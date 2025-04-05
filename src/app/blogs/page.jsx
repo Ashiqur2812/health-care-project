d7a4b02a559ae07b0433cb6c43256a90738b3436
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-05 09:46:52 +0600

    fetch blogs data

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,4 +1,12 @@
-const Blogs = () => {
+export const blogData = async () => {
+  const responsive = await fetch("http://localhost:3000/api/blog/get");
+  const data = await responsive.json();
+  return data;
+};
+
+const Blogs = async () => {
+  const blogs = await blogData();
+  console.log(blogs);
   return (
     <div className="text-gray-900 w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto py-20">
       <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-[#274760]">
